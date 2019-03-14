24a4b453841a3357c1f9fefbb1a263ee8aa02260
Kelly Diabagate <diabagatekelly@yahoo.fr>
2019-03-14 18:15:46 -0500

    Final commit

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,22 +6,40 @@ import { Component, OnInit, Output } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-  @Output() secCount = 0;
-   intervalID;
+  @Output() evenCounter = new EventEmitter();
+  @Output() oddCounter = new EventEmitter();
+  @Output() isOver = new EventEmitter();
+
+  evenSecCount = 0;
+  evenSecCountArray = [];
+  oddSecCountArray = [];
+  evenIntervalID;
+  gameOver;
 
   constructor() {}
 
   ngOnInit() {
   }
 
-  onStartGame($event) {
-   this.intervalID = setInterval(() => {
-      this.secCount++;
+  onStartEvenGame($event) {
+   this.evenIntervalID = setInterval(() => {
+     this.evenSecCount++;
+     if (this.evenSecCount % 2 === 0) {
+      this.evenSecCountArray.push(this.evenSecCount);
+     } else if (this.evenSecCount % 2 !== 0) {
+      this.oddSecCountArray.push(this.evenSecCount);
+     }
+      this.evenCounter.emit(this.evenSecCountArray);
+      this.oddCounter.emit(this.oddSecCountArray);
     }, 1000);
+
+
   }
 
   onEndGame($event) {
-    clearInterval(this.intervalID);
+    clearInterval(this.evenIntervalID);
+    this.gameOver = 'clicked';
+    this.isOver.emit(this.gameOver);
   }
 
 
